12a8ce7d2eb15e0c42b8c4cf6152abade35752cf
Artyom <drrammstein1@gmail.com>
2023-01-28 23:15:25 +0300

    HomeWorkLesson-1

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,55 @@
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+
+    public static void printThreeWords() {
+        String a = "Orange";
+        String b = "Banana";
+        String c = "Apple";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+    public static void checkSumSign() {
+        int a = 40;
+        int b = 2;
+        if (a + b >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+            }
+    }
+
+    public static void printColor() {
+        int value = 62;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        }
+        if (value > 100) {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers(){
+        int a = 10;
+        int b = 10;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+
+}
