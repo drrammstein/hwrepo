656d45e3177a8f79c2c3d7059237079a2f57d7d0
Artyom <drrammstein1@gmail.com>
2023-02-18 15:23:56 +0300

    HomeWorkLesson-2 Task 6

---
+++ src/lesson/HomeWorkLesson2Task6.java
@@ -0,0 +1,11 @@
+package lesson;
+
+public class HomeWorkLesson2Task6 {
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.println(arr[i]);
+        }
+    }
+}
