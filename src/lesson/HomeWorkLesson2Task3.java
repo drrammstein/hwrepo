52dced1c67273653b59114955c31f44f3f1ab20d
Artyom <drrammstein1@gmail.com>
2023-02-18 14:54:13 +0300

    HomeWorkLesson-2 Task 4

--- src/lesson/HomeWorkLesson2Task3.java
+++ src/lesson/HomeWorkLesson2Task3.java
@@ -3,19 +3,18 @@ package lesson;
 import java.util.Scanner;
 
 public class HomeWorkLesson2Task3 {
-    static String str;
     static int a;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите строку");
-        str = sc.nextLine();
-        System.out.println("Введите количество повторов вывода строки");
+        System.out.println("Введите число");
         a = sc.nextInt();
-        repeater(str, a);
+        System.out.print("Результат проверки: " + result(a));
     }
-    public static void repeater(String str, int a){
-        for (int i = 0; i < a; i++) {
-            System.out.println(str);
+    public static boolean result(int a){
+        if (a > -1 ) {
+            return false;
+        } else {
+            return true;
         }
     }
 }
