76894ead0fca6d3a1a9087bd83b7b5edf7569c3b
Artyom <drrammstein1@gmail.com>
2023-02-18 14:53:00 +0300

    HomeWorkLesson-2 Task 4

---
+++ src/lesson/HomeWorkLesson2Task4.java
@@ -0,0 +1,21 @@
+package lesson;
+
+import java.util.Scanner;
+
+public class HomeWorkLesson2Task4 {
+    static String str;
+    static int a;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку");
+        str = sc.nextLine();
+        System.out.println("Введите количество повторов вывода строки");
+        a = sc.nextInt();
+        repeater(str, a);
+    }
+    public static void repeater(String str, int a){
+        for (int i = 0; i < a; i++) {
+            System.out.println(str);
+        }
+    }
+}
